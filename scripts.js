e646517aef0a0c7c59d4278927923b7252bde0cf
Immorpher <58839754+Immorpher@users.noreply.github.com>
2023-10-12 17:30:41 -0400

    Improvement of layout function

--- scripts.js
+++ scripts.js
@@ -1,27 +1,9 @@
-/* W3.JS 1.04 April 2019 by w3schools.com */
-"use strict";
-var w3 = {};
+// Script to generate lauout for website with appropriate folder depth
 
-w3.includeHTML = function(cb) {
-  var z, i, elmnt, file, xhttp;
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          elmnt.removeAttribute("w3-include-html");
-          w3.includeHTML(cb);
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      return;
-    }
-  }
-  if (cb) cb();
-};
+function layout(depth) {
+	let navdata = '\
+	<img src="'+depth+'title.svg" alt="Doom 64 Compendium" style="width: 40rem; height: 8rem;" />\
+	';
+	document.getElementById("navigation").innerHTML = navdata;
+	return;
+}
