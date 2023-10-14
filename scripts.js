0d39ed979a5e6c5b1cc18d72d73770f0f83ee2e3
Immorpher <58839754+Immorpher@users.noreply.github.com>
2023-10-13 23:01:14 -0400

    Improve layout

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@
 
 function layout(depth) {
 	let navdata = '\
-	<img src="'+depth+'title.svg" alt="Doom 64 Compendium" style="width: 40rem; height: 8rem;" />\
+		<img src="'+depth+'title.svg" alt="Doom 64 Compendium" style="width: 15rem; height: 12rem;" />\
+		<a href="#directory">Directory</a>\
+		<a href="#modding">Modding</a>\
+		<a href="#contributors">Contributors</a>\
+		<a href="#affiliates">Affiliates</a>\
 	';
 	document.getElementById("navigation").innerHTML = navdata;
 	return;
