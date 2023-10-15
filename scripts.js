8653931e8586aa0878deca619b3a51d70a22f0e2
Immorpher <58839754+Immorpher@users.noreply.github.com>
2023-10-14 23:41:03 -0400

    Doom 64 EX Wiki Updates

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
 // Script to generate lauout for website with appropriate folder depth
 
 function layout(depth) {
+	
+	// Navigation html
 	let navdata = '\
 		<img src="'+depth+'title.svg" alt="Doom 64 Compendium" style="width: 15rem; height: 12rem;" />\
-		<a href="#directory">Directory</a>\
-		<a href="#modding">Modding</a>\
-		<a href="#contributors">Contributors</a>\
-		<a href="#affiliates">Affiliates</a>\
+		<a href="'+depth+'index.html">Directory</a>\
+		<a href="#engines">Engines</a>\
+		<a href="'+depth+'mapping/index.html">Mapping</a>\
+		<a href="#graphics">Graphics</a>\
+		<a href="#audio">Audio</a>\
+		<a href="'+depth+'/contributors/index.html">Contributors</a>\
+		<a href="#links">Links</a>\
 	';
+	
+	let footdata = '\
+		<p>The Doom 64 Compendum is collaboratively constructed by the <a href="'+depth+'/contributors/index.html">contributors</a>.</p>\
+	';
+	
 	document.getElementById("navigation").innerHTML = navdata;
+	document.getElementById("footer").innerHTML = footdata;
 	return;
 }
